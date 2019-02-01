79c189eac126a0b6f81234e225b5d078c4c55a0d
Rich <rich.oconnor@bigpond.com>
2019-02-01 13:54:20 +1000

    lint

--- buildScripts/server.js
+++ buildScripts/server.js
@@ -2,7 +2,10 @@ import express from "express";
 import path from "path";
 import open from "open";
 import webpack from "webpack";
-import config from "../devenv/webpack.config.dev";
+import config from "../webpack.config.dev";
+
+
+/* eslint-disable no-console */
 
 const port = 3000;
 const app = express();
@@ -14,7 +17,7 @@ app.use(require("webpack-dev-middleware")(compiler, {
 }));
 
 app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "dist/index.html"));
+    res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
 app.listen(port, function(err){
